71686b22b62002ca77d58c1f8ee42691d3ec248c
misiek0n <m.bielecki1909@gmail.com>
2023-04-17 13:03:42 +0200

    zaimportowany numpy, math; poczatek init'u; dodane flh2xyz i xyz2flh - do przerobienia zeby dzialaly jako metody

--- proj1.py
+++ proj1.py
@@ -1,10 +1,38 @@
 import numpy as np
 from math import *
+def Np(f, a, e2):
 
+    N = a / np.sqrt(1 - e2 * sin(f)**2)
+    return(N)
+
+def Mp(f,a,e2):
+    M = a * (1-e2) / np.sqrt((1-e2 * np.sin(f)**2)**3)
+    return(M)
 class Tranformer():
     def __init__(self, model):
-        if model == 'grs80':
-            self.
+        # if model == 'grs80':
+
+    def xyz2flh(self, X, Y, Z, a, e2):
+    P = sqrt(X**2 + Y**2)
+    f = np.arctan(Z/(P*(1 - e2)))
+
+    while True:
+        N = Np(f, a, e2)
+        h = P / cos(f) - N
+        fp = f
+        f = np.arctan(Z/(P* (1 - e2 * N / (N + h))))
+        if abs(fp - f) < (0.000001/206265):
+            break
+
+    l = np.arctan2(Y, X)
+    return(f, l, h)
+
+    def flh2xyz(self, f, l, h, a, e2):
+    N = Np(f, a, e2)
+    X = (N + h) * cos(f) * cos(l)
+    Y = (N + h) * cos(f) * sin(l)
+    Z = (N + h - N * e2) * sin(f)
+    return(X, Y, Z)
 
 
 
