e3022f2032035c98c69c6f97677b666bb0c90c54
misiek0n <m.bielecki1909@gmail.com>
2023-04-17 15:19:55 +0200

    przerobione funkcje pod klase, dodane parametry 2 el. odn.

--- proj1.py
+++ proj1.py
@@ -1,44 +1,46 @@
 import numpy as np
 from math import *
+
+
 def nn(f, a, e2):
     n = a / np.sqrt(1 - e2 * sin(f)**2)
-    return(n)
+    return n
 
-def mp(f,a,e2):
-    M = a * (1-e2) / np.sqrt((1-e2 * np.sin(f)**2)**3)
-    return(M)
-class Transformer():
+
+def mp(f, a, e2):
+    m = a * (1-e2) / np.sqrt((1-e2 * np.sin(f)**2)**3)
+    return m
+
+
+class Transformer:
     def __init__(self, model):
         if model == 'grs80':
             self.a = 6378137
             self.e2 = 0.00669438002290
         elif model == 'wgs84':
             self.a = 6378137
-            self.e2 = 0.0033528106647474805
+            self.e2 = 0.00669438
+        elif model == 'krasowski':
+            self.a = 6378245
+            self.e2 = 0.00669342
 
-    def xyz2flh(self, X, Y, Z):
-        P = sqrt(X**2 + Y**2)
-        f = np.arctan(Z/(P*(1 - self.e2)))
+    def xyz2flh(self, x, y, z):
+        p = sqrt(x**2 + y**2)
+        fi = np.arctan(z/(p*(1 - self.e2)))
 
         while True:
-            n = nn(f, self.a, self.e2)
-            h = P / cos(f) - n
-            fp = f
-            f = np.arctan(Z/(P* (1 - self.e2 * n / (n + h))))
-            if abs(fp - f) < (0.000001/206265):
+            n = nn(fi, self.a, self.e2)
+            h = p / cos(fi) - n
+            fp = fi
+            fi = np.arctan(z/(p * (1 - self.e2 * n / (n + h))))
+            if abs(fp - fi) < (0.000001/206265):
                 break
-    
-            l = np.arctan2(Y, X)
-            return f, l, h
+            la = np.arctan2(y, x)
+            return fi, la, h
 
-    def flh2xyz(self, f, l, h):
-        N = nn(f, self.a, self.e2)
-        X = (N + h) * cos(f) * cos(l)
-        Y = (N + h) * cos(f) * sin(l)
-        Z = (N + h - N * self.e2) * sin(f)
-        return X, Y, Z
-
-
-
-
-
+    def flh2xyz(self, fi, la, h):
+        n = nn(fi, self.a, self.e2)
+        x = (n + h) * cos(fi) * cos(la)
+        y = (n + h) * cos(fi) * sin(la)
+        z = (n + h - n * self.e2) * sin(fi)
+        return x, y, z
