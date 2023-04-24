d52755819fd3dd2749323cc023168258cc59c2b9
Michał Chwałek <michalchwalek@interia.eu>
2023-04-24 13:40:21 +0200

    podmianka funkcji nn i wywóz name main

--- proj1.py
+++ proj1.py
@@ -4,14 +4,8 @@ import argparse
 
 
 
-def nn(f, a, e2):
-    n = a / np.sqrt(1 - e2 * sin(f)**2)
-    return n
 
 
-def mp(f, a, e2):
-    m = a * (1-e2) / np.sqrt((1-e2 * np.sin(f)**2)**3)
-    return m
 
 
 def dms(x):
@@ -43,105 +37,101 @@ class Transformer:
             raise NotImplementedError(f"{model} nie ma takiego modelu :( ")
 
     def xyz2flh(self, x, y, z):
-        if __name__ == "__main__":
-            p = sqrt(x ** 2 + y ** 2)
-            fi = np.arctan(z / (p * (1 - self.e2)))
+        p = sqrt(x ** 2 + y ** 2)
+        fi = np.arctan(z / (p * (1 - self.e2)))
 
-            while True:
-                n = nn(fi, self.a, self.e2)
-                h = p / cos(fi) - n
-                fp = fi
-                fi = np.arctan(z / (p * (1 - self.e2 * n / (n + h))))
-                if abs(fp - fi) < (0.000001 / 206265):
-                    break
+        while True:
+            n = self.a / np.sqrt(1 - self.e2 * sin(f)**2)
+            h = p / cos(fi) - n
+            fp = fi
+            fi = np.arctan(z / (p * (1 - self.e2 * n / (n + h))))
+            if abs(fp - fi) < (0.000001 / 206265):
+                break
 
-            la = np.arctan2(y, x)
-            print(f'Wynik transformacji XYZ -> BLH na elipsoidzie {self.model} to:\n')
-            dms(fi)
-            dms(la)
-            print(f'{h:.3f}')
-            return fi, la, h
+        la = np.arctan2(y, x)
+        print(f'Wynik transformacji XYZ -> BLH na elipsoidzie {self.model} to:\n')
+        dms(fi)
+        dms(la)
+        print(f'{h:.3f}')
+        return fi, la, h
 
     def flh2xyz(self, fi, la, h):
-        if __name__ == "__main__":
-            n = nn(fi, self.a, self.e2)
-            x = (n + h) * cos(fi) * cos(la)
-            y = (n + h) * cos(fi) * sin(la)
-            z = (n + h - n * self.e2) * sin(fi)
-            print(f'Wynik transformacji BLH -> XYZ na elipsoidzie {self.model} to:\n'
-                  f'X = {x:.3f}\n'
-                  f'Y = {y:.3f}\n'
-                  f'Z = {z:.3f}')
-            return x, y, z
+        n = self.a / np.sqrt(1 - self.e2 * sin(f)**2)
+        x = (n + h) * cos(fi) * cos(la)
+        y = (n + h) * cos(fi) * sin(la)
+        z = (n + h - n * self.e2) * sin(fi)
+        print(f'Wynik transformacji BLH -> XYZ na elipsoidzie {self.model} to:\n'
+              f'X = {x:.3f}\n'
+              f'Y = {y:.3f}\n'
+              f'Z = {z:.3f}')
+        return x, y, z
 
     def pl1992(self, fi, lam):
-        if __name__ == "__main__":
-            lam0 = radians(19)
-            m0 = 0.9993
-            b2 = self.a ** 2 * (1 - self.e2)
-            ep2 = (self.a ** 2 - b2) / b2
-            dl = lam - lam0
-            t = tan(fi)
-            n2 = ep2 * cos(fi) ** 2
-            n = nn(fi, self.a, self.e2)
-            a0 = 1 - self.e2 / 4 - 3 * self.e2 ** 2 / 64 - 5 * self.e2 ** 3 / 256
-            a2 = (3 / 8) * (self.e2 + self.e2 ** 2 / 4 + 15 * self.e2 ** 3 / 128)
-            a4 = (15 / 256) * (self.e2 ** 2 + (3 * self.e2 ** 3) / 4)
-            a6 = 35 * self.e2 ** 3 / 3072
-            sigma = self.a * (a0 * fi - a2 * sin(2 * fi) + a4 * sin(4 * fi) - a6 * sin(6 * fi))
-            xgk = sigma + (dl ** 2 / 2) * n * sin(fi) * cos(fi) * \
-                  (1 + (dl ** 2 / 12) * cos(fi) ** 2 * (5 - t ** 2 + 9 * n2 + 4 * n2 ** 2) + ((dl ** 4) / 360) * cos(fi)
-                   ** 4 * (61 - 58 * t ** 2 + t ** 4 + 270 * n2 - 330 * n2 * t ** 2))
-            ygk = dl * n * cos(fi) * (1 + (dl ** 2 / 6) * cos(fi) ** 2 * (1 - t ** 2 + n2) + (dl ** 4 / 120) * cos(fi)
-                                      ** 4 * (5 - 18 * t ** 2 + t ** 4 + 14 * n2 - 58 * n2 * t ** 2))
-            x92 = xgk * m0 - 5300000
-            y92 = ygk * m0 + 500000
-            print(f'Wynik transformacji BL -> XY do układu PL-1992 na elipsoidzie {self.model} to:\n'
-                  f'X = {x92:.3f}\n'
-                  f'Y = {y92:.3f}')
-            return x92, y92
+        lam0 = radians(19)
+        m0 = 0.9993
+        b2 = self.a ** 2 * (1 - self.e2)
+        ep2 = (self.a ** 2 - b2) / b2
+        dl = lam - lam0
+        t = tan(fi)
+        n2 = ep2 * cos(fi) ** 2
+        n = self.a / np.sqrt(1 - self.e2 * sin(f)**2)
+        a0 = 1 - self.e2 / 4 - 3 * self.e2 ** 2 / 64 - 5 * self.e2 ** 3 / 256
+        a2 = (3 / 8) * (self.e2 + self.e2 ** 2 / 4 + 15 * self.e2 ** 3 / 128)
+        a4 = (15 / 256) * (self.e2 ** 2 + (3 * self.e2 ** 3) / 4)
+        a6 = 35 * self.e2 ** 3 / 3072
+        sigma = self.a * (a0 * fi - a2 * sin(2 * fi) + a4 * sin(4 * fi) - a6 * sin(6 * fi))
+        xgk = sigma + (dl ** 2 / 2) * n * sin(fi) * cos(fi) * \
+              (1 + (dl ** 2 / 12) * cos(fi) ** 2 * (5 - t ** 2 + 9 * n2 + 4 * n2 ** 2) + ((dl ** 4) / 360) * cos(fi)
+               ** 4 * (61 - 58 * t ** 2 + t ** 4 + 270 * n2 - 330 * n2 * t ** 2))
+        ygk = dl * n * cos(fi) * (1 + (dl ** 2 / 6) * cos(fi) ** 2 * (1 - t ** 2 + n2) + (dl ** 4 / 120) * cos(fi)
+                                  ** 4 * (5 - 18 * t ** 2 + t ** 4 + 14 * n2 - 58 * n2 * t ** 2))
+        x92 = xgk * m0 - 5300000
+        y92 = ygk * m0 + 500000
+        print(f'Wynik transformacji BL -> XY do układu PL-1992 na elipsoidzie {self.model} to:\n'
+              f'X = {x92:.3f}\n'
+              f'Y = {y92:.3f}')
+        return x92, y92
 
     def pl2000(self, fi, lam):
-        if __name__ == "__main__":
-            if lam <= 16.5:
-                l0 = radians(15)
-                ns = 5
-            elif 16.5 < lam <= 19.5:
-                l0 = radians(18)
-                ns = 6
-            elif 19.5 < lam <= 22.5:
-                l0 = radians(21)
-                ns = 7
-            elif lam > 22.5:
-                l0 = radians(24)
-                ns = 8
-            m0 = 0.999923
-            lam = radians(lam)
-            fi = radians(fi)
-            b2 = self.a ** 2 * (1 - self.e2)
-            ep2 = (self.a ** 2 - b2) / b2
-            dl = lam - l0
-            t = tan(fi)
-            n2 = ep2 * cos(fi) ** 2
-            n = nn(fi, self.a, self.e2)
-            a0 = 1 - self.e2 / 4 - 3 * self.e2 ** 2 / 64 - 5 * self.e2 ** 3 / 256
-            a2 = (3 / 8) * (self.e2 + self.e2 ** 2 / 4 + 15 * self.e2 ** 3 / 128)
-            a4 = (15 / 256) * (self.e2 ** 2 + (3 * self.e2 ** 3) / 4)
-            a6 = 35 * self.e2 ** 3 / 3072
-            sigma = self.a * (a0 * fi - a2 * sin(2 * fi) + a4 * sin(4 * fi) - a6 * sin(6 * fi))
-            xgk = sigma + (dl ** 2 / 2) * n * sin(fi) * cos(fi) * (
-                        1 + (dl ** 2 / 12) * cos(fi) ** 2 * (5 - t ** 2 + 9 * n2 + 4 * n2 ** 2) + (
-                            (dl ** 4) / 360) * cos(fi) ** 4 *
-                        (61 - 58 * t ** 2 + t ** 4 + 270 * n2 - 330 * n2 * t ** 2))
-            ygk = dl * n * cos(fi) * (
-                        1 + (dl ** 2 / 6) * cos(fi) ** 2 * (1 - t ** 2 + n2) + (dl ** 4 / 120) * cos(fi) ** 4 * (
-                            5 - 18 * t ** 2 + t ** 4 + 14 * n2 - 58 * n2 * t ** 2))
-            x2000 = xgk * m0
-            y2000 = ygk * m0 + ns * 1000000 + 500000
-            print(f'Wynik transformacji BL -> XY do układu PL-2000 na elipsoidzie {self.model} to:\n'
-                  f'X = {x2000:.3f}\n'
-                  f'Y = {y2000:.3f}')
-            return x2000, y2000
+        lam = degrees(lam)
+        if lam <= 16.5:
+            l0 = radians(15)
+            ns = 5
+        elif 16.5 < lam <= 19.5:
+            l0 = radians(18)
+            ns = 6
+        elif 19.5 < lam <= 22.5:
+            l0 = radians(21)
+            ns = 7
+        elif lam > 22.5:
+            l0 = radians(24)
+            ns = 8
+        m0 = 0.999923
+        lam = radians(lam)
+        b2 = self.a ** 2 * (1 - self.e2)
+        ep2 = (self.a ** 2 - b2) / b2
+        dl = lam - l0
+        t = tan(fi)
+        n2 = ep2 * cos(fi) ** 2
+        n = self.a / np.sqrt(1 - self.e2 * sin(f)**2)
+        a0 = 1 - self.e2 / 4 - 3 * self.e2 ** 2 / 64 - 5 * self.e2 ** 3 / 256
+        a2 = (3 / 8) * (self.e2 + self.e2 ** 2 / 4 + 15 * self.e2 ** 3 / 128)
+        a4 = (15 / 256) * (self.e2 ** 2 + (3 * self.e2 ** 3) / 4)
+        a6 = 35 * self.e2 ** 3 / 3072
+        sigma = self.a * (a0 * fi - a2 * sin(2 * fi) + a4 * sin(4 * fi) - a6 * sin(6 * fi))
+        xgk = sigma + (dl ** 2 / 2) * n * sin(fi) * cos(fi) * (
+                    1 + (dl ** 2 / 12) * cos(fi) ** 2 * (5 - t ** 2 + 9 * n2 + 4 * n2 ** 2) + (
+                        (dl ** 4) / 360) * cos(fi) ** 4 *
+                    (61 - 58 * t ** 2 + t ** 4 + 270 * n2 - 330 * n2 * t ** 2))
+        ygk = dl * n * cos(fi) * (
+                    1 + (dl ** 2 / 6) * cos(fi) ** 2 * (1 - t ** 2 + n2) + (dl ** 4 / 120) * cos(fi) ** 4 * (
+                        5 - 18 * t ** 2 + t ** 4 + 14 * n2 - 58 * n2 * t ** 2))
+        x2000 = xgk * m0
+        y2000 = ygk * m0 + ns * 1000000 + 500000
+        print(f'Wynik transformacji BL -> XY do układu PL-2000 na elipsoidzie {self.model} to:\n'
+              f'X = {x2000:.3f}\n'
+              f'Y = {y2000:.3f}')
+        return x2000, y2000
 
     def neu(self, fi, lam):
         r = np.array([[-sin(fi) * cos(lam), -sin(lam), cos(fi) * cos(lam)],
